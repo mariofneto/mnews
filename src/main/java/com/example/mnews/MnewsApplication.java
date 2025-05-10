fa22d09857bb4f50f663783f169d06a4e5b7045a
Mário Neto <marioo.netoo3@gmail.com>
2025-05-09 22:00:33 -0300

    iniciando projeto

---
+++ src/main/java/com/example/mnews/MnewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mnews/MnewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
